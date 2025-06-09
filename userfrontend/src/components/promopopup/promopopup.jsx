66363823a47022652f1d6297e002e7b3b360a020
Dharmika Sai Pavuluri <dharmikasai@Dharmikas-MacBook-Air.local>
2025-06-09 23:18:14 +0530

    initial commit

--- userfrontend/src/components/promopopup/promopopup.jsx
+++ userfrontend/src/components/promopopup/promopopup.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './PromoPopup.css';
+import './promopopup.css';
 
 const PromoPopup = () => {
   const [showPopup, setShowPopup] = useState(false);
